1da32f2a9f8b89d423691cc6d6ca92462e079751
FrustratedBoy420 <jaiswalmridul396@gmail.com>
2026-06-08 14:08:09 +0530

    url changes
    Signed-off-by: FrustratedBoy420 <jaiswalmridul396@gmail.com>

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,43 +1,43 @@
-import express from "express";
-import cors from "cors";
-import generateideaplan from "./routes/auth.routes.js";
-import logger from "./utils/logger.js";
-import morgan from "morgan";
-import cookieParser from "cookie-parser";
-const app = express();
+    import express from "express";
+    import cors from "cors";
+    import generateideaplan from "./routes/auth.routes.js";
+    import logger from "./utils/logger.js";
+    import morgan from "morgan";
+    import cookieParser from "cookie-parser";
+    const app = express();
 
-// middleware
-app.use(cors({
-  origin: "https://req-gen-chi.vercel.app/",
-  credentials: true,
-}));
-app.use(express.json());
+    // middleware
+    app.use(cors({
+      origin: "https://req-gen-83ef.vercel.app/",
+      credentials: true,
+    }));
+    app.use(express.json());
 
-const morganFormat = ":method :url :status :response-time ms";
+    const morganFormat = ":method :url :status :response-time ms";
 
-app.use(
-  morgan(morganFormat, {
-    stream: {
-      write: (message) => {
-        const logObject = {
-          method: message.split(" ")[0],
-          url: message.split(" ")[1],
-          status: message.split(" ")[2],
-          responseTime: message.split(" ")[3],
-        };
-        logger.info(JSON.stringify(logObject));
-      },
-    },
-  })
-);
-// test route
+    app.use(
+      morgan(morganFormat, {
+        stream: {
+          write: (message) => {
+            const logObject = {
+              method: message.split(" ")[0],
+              url: message.split(" ")[1],
+              status: message.split(" ")[2],
+              responseTime: message.split(" ")[3],
+            };
+            logger.info(JSON.stringify(logObject));
+          },
+        },
+      })
+    );
+    // test route
 
-app.use(cookieParser());
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+    app.use(cookieParser());
+    app.get("/", (req, res) => {
+      res.send("Hello World");
+    });
 
-// main route
-app.use("/api", generateideaplan);
+    // main route
+    app.use("/api", generateideaplan);
 
-export default app;
+    export default app;
